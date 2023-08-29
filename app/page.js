d8698d9b79e0cff9f32566a0a3f81155e2dd831f
Khaile Tran <khailetran94@gmail.com>
2023-08-29 18:52:52 -0400

    added income modal with descriptions and amount on form

--- app/page.js
+++ app/page.js
@@ -37,65 +37,100 @@ const dummy_data = [
 
 export default function Home() {
 
-  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [showAddIncomeModal, setShowAddIncomeModal] = useState(false);
   
   
-  return ( 
+  return (
     <>
-    {/* Modal */}
+      {/* Add Income Modal */}
 
-    <Modal show = {modalIsOpen} onClose={setModalIsOpen}> 
-    <h3>hello world</h3>
-    </Modal>
+      <Modal show={showAddIncomeModal} onClose={setShowAddIncomeModal}>
+        <form className='flex flex-col gap-4'>
+          <div className='input-group'>
+            <label htmlFor='amount'> Income Amount </label>
+            <input
+              type='number'
+              min={0.1}
+              name ="amount"
+              step={0.1}
+              placeholder='Enter income amount'
+              required
+            />
+          </div>
 
+          <div className='input-group'>
+            <label htmlFor='amount'> Description </label>
+            <input
 
+              type='number'
+              min={0.1}
+              name= "description"
+              step={0.1}
+              placeholder='Enter income amount'
+              required
+            />
+          </div>
 
-  <main className="container max-w-2xl px-6 mx-auto">
-  <section className="py-3">
-    <small className="text-gray-400 text-md"> 
-      My Balance
-    </small>
-    <h2 className ="text-4xl font-bold">{currencyFormatter(100000)}</h2>
-  </section>
+          <button type = "submit" className='btn btn-primary'>Add Transaction</button>
+        </form>
+      </Modal>
 
-  <section className="flex items-center gap-2 py-3">
-    <button onClick= {() => {setModalIsOpen(true)}}className="btn btn-primary">- Expenses</button>
-    <button className="btn btn-primary-outline"> + Income</button>
-  </section>
-  {/* Expenses */}
-  <section className='py-6'>
-    <h3 className='text-2xl'>My Expenses</h3>
-    <div className='flex flex-col gap-4 mt-6'>
-    {dummy_data.map(expense => {
-      return (
-      <ExpenseItem 
-      color={expense.color} 
-      title ={expense.title}
-      total = {expense.total}
-      />
-      );
-     })}
-    </div>
-  </section>
+      <main className='container max-w-2xl px-6 mx-auto'>
+        <section className='py-3'>
+          <small className='text-gray-400 text-md'>My Balance</small>
+          <h2 className='text-4xl font-bold'>{currencyFormatter(100000)}</h2>
+        </section>
 
-  {/* Chart Session */}
-  <section className='py-6'></section>
-  <h3 className='text-2xl'> Stats </h3>
-  <div className='w-1/2 mx-auto '>
-    <Doughnut data= {{
-      labels: dummy_data.map(expense => expense.title),
-      datasets: [
-        {
-          label: "Expenses",
-          data: dummy_data.map (expense => expense.total),
-          backgroundColor: dummy_data.map(expense => expense.color),
-          borderColor: ['#18181b'],
-          borderWidth: 5,
-        }
-      ]
-    }}/>
-  </div>
-  </main>
-      </>
+        <section className='flex items-center gap-2 py-3'>
+          <button onClick={() => {}} className='btn btn-primary'>
+            - Expenses
+          </button>
+          <button
+            onClick={() => {
+              setShowAddIncomeModal(true);
+            }}
+            className='btn btn-primary-outline'
+          >
+            {' '}
+            + Income
+          </button>
+        </section>
+        {/* Expenses */}
+        <section className='py-6'>
+          <h3 className='text-2xl'>My Expenses</h3>
+          <div className='flex flex-col gap-4 mt-6'>
+            {dummy_data.map(expense => {
+              return (
+                <ExpenseItem
+                  color={expense.color}
+                  title={expense.title}
+                  total={expense.total}
+                />
+              );
+            })}
+          </div>
+        </section>
+
+        {/* Chart Session */}
+        <section className='py-6'></section>
+        <h3 className='text-2xl'> Stats </h3>
+        <div className='w-1/2 mx-auto '>
+          <Doughnut
+            data={{
+              labels: dummy_data.map(expense => expense.title),
+              datasets: [
+                {
+                  label: 'Expenses',
+                  data: dummy_data.map(expense => expense.total),
+                  backgroundColor: dummy_data.map(expense => expense.color),
+                  borderColor: ['#18181b'],
+                  borderWidth: 5,
+                },
+              ],
+            }}
+          />
+        </div>
+      </main>
+    </>
   );
 }
