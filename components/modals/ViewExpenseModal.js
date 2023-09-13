29b632a15bf3694b2ca090fa0e2b2f82a0ae1c89
Khaile Tran <khailetran94@gmail.com>
2023-09-13 15:00:57 -0400

    added in functionality to delete expense item in db and frontend

--- components/modals/ViewExpenseModal.js
+++ components/modals/ViewExpenseModal.js
@@ -1,5 +1,39 @@
 import Modal from '@/components/Modal';
+import { currencyFormatter } from '@/lib/utils';
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useContext } from 'react';
+import { financeContext } from '@/lib/store/finance-context';
+import { useAmp } from 'next/amp';
+
+
 function ViewExpenseModal({ show, onClose, expense }) {
+
+  const {deleteExpenseItem} = useContext(financeContext)
+
+  const deleteExpenseItemHandler = async (item) => {
+    try {
+      //remove item from the array
+      //filter the item from the array
+      //keep the item if id is not equal to item id
+      //updating the state of item
+      const updatedItems = expense.items.filter((i) => 
+        i.id !== item.id);
+
+      const updatedExpense = {
+        items: [...updatedItems],
+        total: expense.total - item.amount,
+       }
+
+       await deleteExpenseItem(updatedExpense, expense.id);
+
+     }catch(error){
+      console.log(error.message)
+      }
+   }
+
+
+
+
   return (
     <Modal show={show} onClose={onClose}>
       <div className='flex items-center justify-between'>
@@ -10,15 +44,24 @@ function ViewExpenseModal({ show, onClose, expense }) {
       <div>
         <h3 className='my-4 text-2xl'>Expense Transactions</h3>
         {expense.items.map((item)=> {
-            return <div 
+            return (<div 
             key = {item.id}
             className='flex items-center justify-between'>
-             <small>{item.createdAt.toMillis
+             <small>
+              {item.createdAt.toMillis
              ? new Date (item.createdAt.toMillis()).toISOString()
              : item.createdAt.toISOString() }
              </small>
-
+            <p className='flex items-center gap-2'>
+              {currencyFormatter(item.amount)}
+              <button onClick = {()=> {
+                deleteExpenseItemHandler(item);
+                 }}>
+                <FaRegTrashAlt />
+              </button>
+            </p>
             </div>
+            );
          })}
       </div>
     </Modal>
