bd5f89434d7d5ebc71a57b3bf9a9d09037d79040
Khaile Tran <khailetran94@gmail.com>
2023-10-06 10:49:57 -0400

    added in styling for sign in page

--- components/Signin.js
+++ components/Signin.js
@@ -6,24 +6,26 @@ import { authContext } from '@/lib/store/auth-context';
 function SignIn() {
     const { googleLoginHandler } = useContext(authContext);
   return (
-    <main className='container max-w-2xl px-6 mx-auto'>
-      <h1 className='mb-6 text-6xl font-bold text-center'>Welcome 👋</h1>
-
+    <div className='login-component min-h-screen flex flex-col items-center justify-center'>
+    <main className=' container max-w-2xl px-6 mx-auto '>
       <div className='flex flex-col overflow-hidden shadow-md shadow-slate-500 bg-slate-800 rounded-2xl'>
-        <div className='h-52'>
+        <div className='h-100'>
           <img
-            className='object-cover w-full h-full'
-            src='https://images.pexels.com/photos/4386431/pexels-photo-4386431.jpeg'
+            className='object-contain'
+            src='https://i.imgur.com/7FlDLac.png'
           />
         </div>
         <div className='px-4 py-4'>
-        <h3 className='text-2xl text-center'>Sign In</h3>
+        <h3 className='mb-6 text-3xl font-bold text-center'>Sign In</h3>
+        
         <button onClick = {googleLoginHandler} className="flex self-start gap-2 p-4 mx-auto mt-6 font-medium text-white align-middle bg-gray-700 rounded-lg">
         <FcGoogle className="text-2xl" /> Google
         </button>
         </div>
       </div>
     </main>
+
+    </div>
   );
 }
 
